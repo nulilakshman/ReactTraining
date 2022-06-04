9d0d9faf855c95912b2e32a3f5aa852253339c13
nuli.lakshman <nuli.lakshman@gmail.com>
2022-06-04 17:52:34 +0530

    Added styling

---
+++ ecommerce/src/Components/AddProducts.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react"
+import {
+    Button,
+    Row,
+    Col,
+    Form,
+    CardBody,
+    Card,
+    FormGroup,
+    Label,
+    Container,
+    CardTitle,
+    Input
+} from "reactstrap"
+
+const AddProducts = () => {
+    return (
+        <React.Fragment>
+            
+            <div id="layout-wrapper">
+                <h2>Thanos</h2>
+
+                <div className="main-content">
+
+
+                    <div className="page-content">
+                        <Container fluid>
+                            <Row>
+                                <Col lg="12">
+                                    <Card>
+                                        <CardBody>
+                                            <CardTitle>Create Inventory Request</CardTitle>
+                                            <Form>
+                                                <Row>
+                                                    <Col lg="6">
+                                                        <Label for="userName" className="col-sm-6 col-form-Label">
+                                                            UserName *
+                                                        </Label>
+                                                        <Input
+                                                            type="text"
+                                                            className="form-control"
+                                                            id="name"
+                                                            name="name"
+                                                        />
+                                                    </Col>
+                                                    <Col lg="6">
+                                                        <Label for="userName" className="col-sm-6 col-form-Label">
+                                                            UserName *
+                                                        </Label>
+                                                        <Input
+                                                            type="text"
+                                                            className="form-control"
+                                                            id="name"
+                                                            name="name"
+                                                        />
+                                                    </Col>
+                                                </Row>
+                                            </Form>
+                                        </CardBody>
+
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+
+            </div>
+
+           
+        </React.Fragment>
+    )
+}
+
+export default AddProducts
---
+++ ecommerce/src/Components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div class="header">
+            <a href="#default" class="logo">CompanyLogo</a>
+            <div class="header-right">
+                <a class="active" href="#home">Home</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
